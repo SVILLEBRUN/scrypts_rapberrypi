d313e6e64fb724134091279547a4196f4435eb23
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-07-10 18:44:06 +0200

    Remove commented line

--- ssh_monitor.sh
+++ ssh_monitor.sh
@@ -89,7 +89,6 @@ cleanup() {
   echo
   echo "Stop requested, killing processes..."
   kill  "$AUTHLOG_PID" "$FAIL2BAN_PID"
-  # Run cron script before exiting
   bash "$HOME/fail2ban/ssh_monitor_cron.sh"
   exit 0
 }
